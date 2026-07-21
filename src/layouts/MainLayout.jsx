ced0bfe00a4e317030fd045fbbd7c975848800f4
kasraaghayari <kasraaghayari@gmail.com>
2026-07-21 19:28:27 +0330

    before adding pages

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,21 @@
+import {Outlet} from "react-router-dom"
+import Footer from "../components/Footer/Footer"
+import Background from "../components/Background/Background"
+import Navbar from "../components/Navbar/Navbar"
+
+import styles from "./MainLayout.module.css"
+
+export default function MainLayout () {
+    return(
+        <>
+            <Background />
+            
+            <main className={styles.container}>
+                <Outlet/>
+                <Navbar/>
+                <Footer/>
+            </main>
+            
+        </>
+    )
+}
---
+++ src/routes/Router.jsx
@@ -0,0 +1,37 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+/* -------------------------------------------------------------------------- */
+/*                                    صفحات                                   */
+/* -------------------------------------------------------------------------- */
+import MainLayout from "../layouts/MainLayout"
+import Home from "../pages/Home/Home"
+import AboutUs from "../pages/AboutUs/AboutUs"
+import Contact from "../pages/Contact/Contact"
+import Profile from "../pages/Profile/Profile"
+
+export default function Router() {
+    return(
+        <BrowserRouter>
+        
+            <Routes>
+
+                <Route element={<MainLayout />}>
+                    <Route path="/" element={<Home />} /> 
+                    <Route path="/about" element={<AboutUs />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/profile" element={<Profile />} /> 
+                </Route> 
+
+                {/* <Route element={<AuthLayout />}>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Route>
+
+                <Route element={<ChatLayout />}>
+                    <Route path="/chat" element={<Chat />} />
+                </Route> */}
+
+            </Routes>
+        </BrowserRouter>
+    )
+}
